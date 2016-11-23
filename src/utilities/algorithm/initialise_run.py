f43f1b8899c2f3c833a278c0a34c0e10bad16f28
mikefenton <michaelfenton1@gmail.com>
2016-11-23 15:50:09 +0000

    Updated python version check to ensure minimum of python 3.5 is used.

--- src/utilities/algorithm/initialise_run.py
+++ src/utilities/algorithm/initialise_run.py
@@ -14,8 +14,9 @@ def check_python_version():
     :return: Nothing
     """
 
-    if version_info.major < 3:
-        print("\nError: Python version not supported. Must use Python 3.x")
+    if version_info.major < 3 or version_info.minor < 5:
+        print("\nError: Python version not supported. Must use at least "
+              "Python 3.5")
         quit()
 
 
