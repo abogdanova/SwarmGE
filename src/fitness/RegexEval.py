6ea57de6384d839802b172b9290493eb1e35f966
Brendan (dhcp-892b6a81) <codykenny@gmail.com>
2016-11-28 11:12:59 +0000

    It helps to separate multiple matches in a single string into strings with a single match

--- src/fitness/RegexEval.py
+++ src/fitness/RegexEval.py
@@ -40,10 +40,10 @@ class RegexEval:
 #                print(regex_string + ": {}".format(fitness))
 #                sys.exit()
             if fitness >= 0: # (we should use multi-objective/pareto front)
-                return fitness # error is first, time second
+                return fitness + (len(regex_string)/10000000000) #fitness # error is first, length second
             else:
                 self.time=True
-                return fitness + (len(regex_string)/10000000000)  # auto-tune this number so that performance first, length second
+                return fitness # + (len(regex_string)/10000000000)  # performance only 
 
         except:
 #            print(traceback.format_exc())
@@ -59,11 +59,11 @@ class RegexEval:
                 result_error += 100 * (len(a_result[3].search_string)) #+ len(a_result[3].matched_string))
             else: # a match which may be the empty string
                 result_error += a_result[3].calc_match_errors(a_result[1])
-        if result_error:
+        if result_error >1:
             fitness = result_error
         else:
             fitness = time_sum
-        # fitness = time_sum + result_error
+        #fitness = time_sum + result_error
         # if fitness == seed_fitness:
         # fitness = 100 * len(a_result) # identical result to seed penalised (plucking the centre from spiderweb)
         return fitness
@@ -108,8 +108,12 @@ class RegexEval:
         a_test_string = RegexTestString("Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: \"none\" (in: eth0 119.63.193.196(5c:0a:5b:63:4a:82):4399 -> 140.105.63.164(50:06:04:92:53:44):80 TCP flags: ****S* len:60 ttl:32)")
         a_test_string.add_match(119,136) # 5c:0a:5b:63:4a:82
         a_test_string.add_match(161,178) # 50:06:04:92:53:44
-
         self.test_cases.append(a_test_string)
+
+        a_test_string = RegexTestString(" -> 140.105.63.164(50:06:04:92:53:44):80 TCP flags: ****S* len:60 ttl:32)")
+        a_test_string.add_match(19,36) # 50:06:04:92:53:44
+        self.test_cases.append(a_test_string)
+
         
 class RegexTestString:
     def __init__(self,search_string):
@@ -152,18 +156,23 @@ class RegexTestString:
                     match_error +=1
                 
             # missing any of the desired extraction costs a lot
-            match_error += 5 * self.find_missing_range(a_known_match.get("start"), a_known_match.get("end"), match_ranges)
+            missing_range = self.find_missing_range(a_known_match.get("start"), a_known_match.get("end"), match_ranges)
+            range_length = (a_known_match.get("end") - a_known_match.get("start"))
+            if missing_range == range_length: # we've missed the whole thing
+                match_error += missing_range * (range_length * 10)
+            else:
+                match_error += missing_range * range_length
             
             #if len(start_errors) == 0:
             #    start_errors.append(len(self.search_string))
             #if len(end_errors) == 0:
             #    end_errors.append(len(self.search_string))
         if match_candidates_length < 0:
-            match_candidates_length = match_error = 5*len(self.search_string)
+            match_candidates_length = match_error = 50*len(self.search_string)
         #print("Matches: {}".format(match_candidates_length))
         #if (sum(start_errors+end_errors) + match_error) == 0:
         #    print("aagh")
-        return (match_error) + match_candidates_length # sum(start_errors+end_errors) + 
+        return (match_error) + (match_candidates_length * 5) # sum(start_errors+end_errors) + 
 
     def find_missing_range(self,start, end, match_ranges):
         missing = end - start
