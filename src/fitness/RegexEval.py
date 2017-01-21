92282bc2b15b9ecaf144f0d27627d3916ada5e77
Brendan Cody-Kenny <codykenny@gmail.com>
2017-01-21 23:25:41 +0100

    Cleaned up the code

--- src/fitness/RegexEval.py
+++ src/fitness/RegexEval.py
@@ -5,7 +5,6 @@ import sys
 from algorithm.parameters import params
 from representation import individual
 from multiprocessing import Process, Queue
-#from multiprocessing.pool import ThreadPool # could be better performance to keep the thread around (is overhead cost of recreating a thread, less than the cost of exponential regex? no)
 
 # http://stackoverflow.com/questions/24812253/how-can-i-capture-return-value-with-python-timeit-module/
 timeit.template = """
@@ -22,49 +21,46 @@ def inner(_it, _timer{init}):
 TODO
 Apache log file
 ([0-9a-f.:]+)\s+(-|.+?)\s+(-|.+?)\s+\[([0-9]{2}\/[a-z]{3}\/[0-9]{4}\:[0-9]{2}:[0-9]{2}:[0-9]{2}[^\]]*)\] \"(\S+?)\s(\S*?)\s{0,1}(\S+?)\" ([0-9|\-]+) ([0-9|\-]+)
-
 """
 
+# from multiprocessing.pool import ThreadPool
+# could be better performance to keep the thread around (is overhead cost of recreating a thread, less than the cost of exponential regex? no)
 # pool = ThreadPool(processes=1)
 
-class RegexEval:
-    """
-    Fitness function for checking regex matching which sums functionality error.
-    The regex is presented with a number of strings.
-    The resulting matched values are checked against known correct answers
-    """
 
-    maximise = False
+"""
+Fitness function for regex (lower fitness value is better)
+Fitness = functionality error + time
+The regex is presented with a number of strings, resulting matches are checked for correctness against known correct answers.
+Sum of wall-clock time taken to execute the test strings. 
+"""
+
+class RegexEval:
+    maximise = False # lower fitness value is better
     
     def __init__(self):
         self.test_cases = list()
         self.generate_tests()
         self.time=True
 
-
+    """
+    This method is called when this class is instantiated with an individual (a regex)
+    """    
     def call_fitness(self, individual, q):
         regex_string = individual.phenotype
-#        regex_string = "^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$"
-#        regex_string = "^\s*(-|\+)?(\d+|(\d*(\.\d*)))([eE][+-]?\d+)?\s*$"
-        #regex_string = "\d.*\w.$|e"
-        #        regex_string="!|\w.[6-l]\w\w[^!]\w\w[^!]\w\w[^!]\d\w[^P]\w\w"
         try:
             compiled_regex = re.compile(regex_string)
             eval_results = self.test_regex(compiled_regex)
             result_error,time_sum = self.calculate_fitness(eval_results)
-            #            if "5" in regex_string:
-            # (we should use multi-objective/pareto front)
-            #print(regex_string + ": {}".format(fitness))
-            #sys.exit()
             fitness = result_error + time_sum
-#            if fitness == 0 : # no error
-#                fitness += time_sum
-            if 'SEED_GENOME' in params and params['SEED_GENOME']:
-                similarity_score = self.calculate_similarity_score(regex_string)
-                #if(similarity_score > 1):
-                #    fitness += (similarity_score)
-                #else:
-                #    fitness += time_sum
+
+            # check how similar the mutated regex is to the seed regex, character by character. Use Levenshtein distance either?
+            # if 'SEED_GENOME' in params and params['SEED_GENOME']:
+            #    similarity_score = self.calculate_similarity_score(regex_string)
+            #    if(similarity_score > 1):
+            #        fitness += (similarity_score)
+            #    else:
+            #        fitness += time_sum
 
 
             # phenotype or genome length?
@@ -75,15 +71,15 @@ class RegexEval:
             # else:
             #     self.time=True
             #     return fitness
-            q.put(fitness)
-#            print("\n {}".format(fitness))
+            
+            q.put(fitness) # We are running this code in a thread so put the fitness on the queue so it can be read back by the first
 
         except:
-            #print(" ------------ exception")
-#            print(traceback.format_exc())
-#            sys.exit()
-            q.put(100000)
+            q.put(100000) # if the regex is broken, or the thread is timedout, return a really bad fitness
 
+    """
+    Check how similar a mutated regex is to the seed regex. It is an alternative measure to Levenshtein distance (experimental) 
+    """
     def calculate_similarity_score(self, regex_string):
         char_same_count = 0
         if 'SEED_GENOME' in params and params['SEED_GENOME']:
@@ -96,46 +92,39 @@ class RegexEval:
                     if regex_string[i] == (seed_string)[j]:
                         char_same_count += 1
                         break
-        #char_same_count += (abs(len(seed_string) - len(regex_string))/100)
-        return char_same_count # / len(seed_string)
-         
+        return char_same_count 
 
+    
+    """
+    Sum the functionality error with time (and any other fitness penalties you want to add, e.g. length of regex)
+    """
     def calculate_fitness(self,eval_results):
         result_error=0
         time_sum=0.0
-        # print(" ")
-        # print("Calculate_fitness ")
         for a_result in eval_results:
-            time_sum += a_result[0] # /  a_result[2]
-            # print("time_val : {} ".format(a_result[0]))
-            # if a_result[1] == None: # no match
-            # result_error += 100 * (len(a_result[3].search_string)) #+ len(a_result[3].matched_string))
-            # else: # a match which may be the empty string
+            time_sum += a_result[0] 
             result_error += a_result[3].calc_match_errors(list(a_result[1]))
-        #if result_error >1:
-        #    fitness = result_error
-        #else:
-        #    fitness = time_sum
-        # fitness = result_error + time_sum
         # if fitness == seed_fitness:
-        # fitness = 100 * len(a_result) # identical result to seed penalised (plucking the centre from spiderweb)
+        #     fitness = 100 * len(a_result) # identical result to seed penalised
         return result_error,time_sum
 
+    """
+    Iterate through test cases
+    """
     def test_regex(self,compiled_regex):
         results = list()
         testing_iterations=1
         # do a quick test to time the longest test case (which is also the last in the list)
         quick_test = self.time_regex_test_case(compiled_regex, self.test_cases[len(self.test_cases)-1], testing_iterations)
-        #        print("quick_test time: {}".format(quick_test[0]))
-        # aim for entire test suite to take less than a second
         if quick_test[3].calc_match_errors(list(quick_test[1])) < 0 : # Ideally we only time a program if it is funtionally correct
-            # eval_time = .05 # seconds
-            testing_iterations = 10000000 # int(( eval_time / (quick_test[0]/10))/len(self.test_cases)) # change to half second?
-        # print("Iterations {}".format(testing_iterations))
+            testing_iterations = 10000000 
         for test_case in self.test_cases:
             results.append(self.time_regex_test_case(compiled_regex, test_case, testing_iterations))
         return results
 
+    """
+    Execute and time a single regex on a single test case
+    """
     def time_regex_test_case(self, compiled_regex, test_case, iterations):
         repeats = 3
         iterations_per_repeat = iterations
@@ -146,15 +135,11 @@ class RegexEval:
             return list(compiled_regex.finditer(search_string)) 
         
         t = timeit.Timer(wrap)
-
         repeat_iterations = t.repeat(repeat=repeats, number = iterations)
-        
         best_run = list(repeat_iterations[0])
         for repeated_timeit in repeat_iterations:
             if best_run[0] > list(repeated_timeit)[0]:
                 best_run = list(repeated_timeit)
-        #return_tuple = t.timeit(number = iterations)
-        #return_vals = t.timeit()
         return_vals = list(best_run)
         return_vals.append(iterations)
         return_vals.append(test_case)
@@ -180,28 +165,10 @@ class RegexEval:
                     if len(list(vals[1])) == 0:
                         self.test_cases.append(a_test_case_string)
 
-
-    def generate_equivalence_test_suite_length(self, a_match, a_regex):
-        # improve performance of a bad regex.
-        # taken from here: https://www.loggly.com/blog/regexes-the-bad-better-best/
-        
-        # best (by hand) regex: [12]\d{3}-[01]\d-[0-3]\d ([^ \[]*?)\[([^\]]*?)\]:.*
-        """
-        Technically this regex is still not the “best,” as there are even
-        more optimizations that you can make. Those optimizations, in
-        my opinion, greatly complicate the regex and take quite a lot
-        of time and expertise to craft, but they do boost the
-        performance of the regex by a non-trivial amount. The
-        optimizations I describe in this post give by far the greatest
-        bang for your buck performance-wise while maintaining the
-        readability of the regex. If you’d like to learn more about
-        how to optimize even further, check out this
-        post:http://www.rexegg.com/regex-quantifiers.html#explicit_greed.
-        """
-        # worst regex: .* (.*)\[(.*)\]:.*
-        # seed genome to go in regex.txt parameters file
-        # [10082, 41053, 89472, 13943, 83234, 23739, 51472, 59723, 52910, 4880, 72071, 68514, 53866, 25754, 39758, 51472, 59723, 29655, 22034, 52910, 4880, 72071, 5630, 83669, 54528, 45876, 76718, 53866, 25754, 39758, 51472, 59723, 29655, 22034, 52910, 4880, 72071, 56000, 67442, 49372, 13302, 70567, 37415, 81973, 40820, 18637, 88657, 51472, 59723, 52910, 4880, 72071]
-        
+    """
+    Generate shorter or longer test cases, add those which do not match
+    """
+    def generate_equivalence_test_suite_length(self, a_match, a_regex):        
         # add and remove characters from the string until we find a regex which fails
         compiled_regex = re.compile(a_regex)
         if len(a_match.matches) > 0 :
@@ -217,7 +184,9 @@ class RegexEval:
                 new_search_string = a_match.search_string[:i] # TODO: refactor this
                 self.add_test_case_if_fails(new_search_string, compiled_regex)
 
-
+    """
+    run a test case, if it fails, add it to the suite of tests
+    """
     def add_test_case_if_fails(self,new_search_string, compiled_regex):
         a_test_case_string = RegexTestString(new_search_string)
         vals = self.time_regex_test_case(compiled_regex, a_test_case_string, 1)
@@ -225,6 +194,9 @@ class RegexEval:
             self.test_cases.append(a_test_case_string)
 
 
+    """
+    Add a test safely
+    """
     def add_test(self,regex_string, match):
         a_test_string = RegexTestString(regex_string)
         if match and len(match) == 2: # should be e.g. [0,27]
@@ -239,8 +211,6 @@ class RegexEval:
         # target: ^\d{4,}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+(?:[+-][0-2]\d:[0-5]\d|Z)$
         a_test_string = self.add_test("2016-12-09T08:21:15.9+00:00",[0,27])
         self.generate_equivalence_test_suite_replacement(a_test_string,"^\d{4,}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+(?:[+-][0-2]\d:[0-5]\d|Z)$")
-
-        # this does not match at all! (what will our fitness function throw?)
         self.add_test("2016-12-09T08:21:15.9+00:0", None)
         self.add_test("2016-22-09T08:21:15.9+00:00000000000",None) # 
         self.add_test("2016-22-09T08:21:15.9+00:00",None) # no match, as 22 is not a valid month
@@ -324,8 +294,7 @@ class RegexEval:
         
     def generate_regex_mac_search_string_tests(self):
         #a_test_string = RegexTestString("Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: \"none\" (in: eth0 119.63.193.196(5c:0a:5b:63:4a:82):4399 -> 14") 
-       #a_test_string.add_match(119,136) # 5c:0a:5b:63:4a:82
-
+        #a_test_string.add_match(119,136) # 5c:0a:5b:63:4a:82
         a_test_string = RegexTestString("Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: \"none\" (in: eth0 119.63.193.196(5c:0a:5b:63:4a:82):4399 -> 140.105.63.164(50:06:04:92:53:44):80 TCP flags: ****S* len:60 ttl")
         a_test_string.add_match(119,136) # 5c:0a:5b:63:4a:82
         a_test_string.add_match(161,178) # 50:06:04:92:53:44
@@ -412,7 +381,9 @@ class RegexEval:
         a_test_string.add_match(0,17)
         self.test_cases.append(a_test_string)
 
-        
+    """
+    Generate tests, as specified in the parameters file
+    """    
     def generate_tests(self):
         test_gen_method = getattr(self, params['FITNESS_TEST_SUITE'])
         test_gen_method()
@@ -426,14 +397,16 @@ class RegexEval:
         # self.generate_PonyGE2_Grammar_File_Rule_tests()
         # self.generate_catastrophic_QT3TS_tests()
         # self.generate_d3_interpolate_number()
-
         print("Number of test cases: {}".format(len(self.test_cases)))
 
+    """
+    When this class is instantiated, evaluted in a new process, timeout and kill process if it runs for 5 seconds
+    Generating new processes is expensive, rework the code to reuse a process.
+    """
     def __call__(self, individual):
         q = Queue()
         p = Process(target=self.call_fitness, name="self.call_fitness", args=(individual,q))
         p.start()
-
         p.join(5)
         
         # If thread is active
@@ -445,11 +418,33 @@ class RegexEval:
             return 100000
         else:
             fitness = q.get()
-#            print("\n again: {}".format(fitness))            
             return fitness
 
-            
+        
+        # improve performance of a bad regex.
+        # taken from here: https://www.loggly.com/blog/regexes-the-bad-better-best/        
+        # best (by hand) regex: [12]\d{3}-[01]\d-[0-3]\d ([^ \[]*?)\[([^\]]*?)\]:.*
+        """
+        Technically this regex is still not the “best,” as there are even
+        more optimizations that you can make. Those optimizations, in
+        my opinion, greatly complicate the regex and take quite a lot
+        of time and expertise to craft, but they do boost the
+        performance of the regex by a non-trivial amount. The
+        optimizations I describe in this post give by far the greatest
+        bang for your buck performance-wise while maintaining the
+        readability of the regex. If you’d like to learn more about
+        how to optimize even further, check out this
+        post:http://www.rexegg.com/regex-quantifiers.html#explicit_greed.
+        """
+        # worst regex: .* (.*)\[(.*)\]:.*
+        # seed genome to go in regex.txt parameters file
+        # [10082, 41053, 89472, 13943, 83234, 23739, 51472, 59723, 52910, 4880, 72071, 68514, 53866, 25754, 39758, 51472, 59723, 29655, 22034, 52910, 4880, 72071, 5630, 83669, 54528, 45876, 76718, 53866, 25754, 39758, 51472, 59723, 29655, 22034, 52910, 4880, 72071, 56000, 67442, 49372, 13302, 70567, 37415, 81973, 40820, 18637, 88657, 51472, 59723, 52910, 4880, 72071]
 
+        
+"""
+Class which contains a test string and matches
+(We should probably just use the types which re library uses)
+"""
 class RegexTestString:
     def __init__(self,search_string):
         # print("Added regex search string: "+search_string)
@@ -458,16 +453,10 @@ class RegexTestString:
 
     def add_match(self, start, end):
         self.matches.append({'start': start,'end': end, 'matched_string': self.search_string[start:end]}) # ew?
-        # print("Added the following match: "+self.matches[-1].get("matched_string"))
-#         for i in range(start,end-1):
-#             for j in range(start+1,end):
-#                 self.match.append(start=i,end=j, matched_string=self.search_string[i:j])
         
     def calc_match_errors(self,match_candidates):
         match_ranges=list()
         undesired_range = missing_range = 0
-        #        for match in match_candidates:
-        #           match_ranges.append(match)
 
         for a_known_match in self.matches:
             # missing any of the desired extraction costs a lot
@@ -505,26 +494,6 @@ class RegexTestString:
             if not in_range:
                 undesired_matched += 1
         return undesired_matched
-
-
-    #             for a_match_candidate in match_candidates: # how much of our known match has gone unmatched?
-#                 temp_match=0
-#                 if a_matched_candidate.start < a_known_match.end or a_matched_candidate.end > a_known_match.start:
-#                     unmatched_start = a_matched_candidate.start - a_known_match.start # should be positive, if negative then there is none unmatched
-#                     unmatched_end = a_matched_candidate.end - a_known_match.start # should be positive
-#                     temp_match = max(unmatched_end,0) + max(unmatched_start,0)
-
-
-        
-#     def compare(self,attempt_string):
-#         error = distance(self.matched_string, attempt_string.group(0))
-#         attempti=0
-#         attempti += self.search_string.find(attempt_string.group(0))
-#         if attempti < self.starti:
-#             error+= self.starti-attempti
-#         elif attempti > self.endi:
-#             error+= attempti-self.endi
-#         return error
 
     def get_search_string(self):
         return self.search_string
