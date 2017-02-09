ba237db16666f2cae61abb46c2b3763705028c83
James McDermott <jamesmichaelmcdermott@gmail.com>
2017-02-09 14:53:43 +0000

    Added reference for Max problem.

--- src/fitness/pymax.py
+++ src/fitness/pymax.py
@@ -3,10 +3,16 @@ from algorithm.parameters import params
 class pymax:
     """Py-max is a max-style problem where the goal is to generate a
     function which outputs a large number. In the standard GP Max
-    problem this function can only use the constant (0.5) and
-    functions (+, *). The Py-max problem allows more programming:
-    numerical expressions, assignment statements and loops. See
-    pymax.pybnf.
+    [Gathercole and Ross] problem this function can only use the
+    constant (0.5) and functions (+, *). The Py-max problem allows
+    more programming: numerical expressions, assignment statements and
+    loops. See pymax.pybnf.
+
+    Chris Gathercole and Peter Ross. An adverse interaction between
+    crossover and restricted tree depth in genetic programming. In
+    John R. Koza, David E. Goldberg, David B. Fogel, and Rick
+    L. Riolo, editors, Genetic Programming 1996: Proceedings of the
+    First Annual Conference.
 
     """
 
