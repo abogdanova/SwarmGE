bf1a39869c8daaeebc65dc410e4b909cd9c19c86
mikefenton <michaelfenton1@gmail.com>
2017-02-14 17:37:58 +0000

    Removed print statements from pymax fitness function and cleaned it up slightly.

--- src/fitness/pymax.py
+++ src/fitness/pymax.py
@@ -1,5 +1,3 @@
-from algorithm.parameters import params
-
 class pymax:
     """Py-max is a max-style problem where the goal is to generate a
     function which outputs a large number. In the standard GP Max
@@ -16,17 +14,19 @@ class pymax:
 
     """
 
-
-    maximise = True # True as it ever was
+    maximise = True  # True as it ever was.
 
     def __call__(self, ind):
-        # ind.phenotype will be a string incl fn defns etc. when we
-        # exec it will create a value XXX_output_XXX, but we exec
+        # ind.phenotype will be a string, including function definitions etc.
+        # When we exec it, it will create a value XXX_output_XXX, but we exec
         # inside an empty dict for safety.
-        p = ind.phenotype
-        print(p)
-        print("")
-        d = {}
+        
+        p, d = ind.phenotype, {}
+        
+        # Exec the phenotype.
         exec(p, d)
-        s = d['XXX_output_XXX'] # this is the program's output: a number
+        
+        # Get the output
+        s = d['XXX_output_XXX']  # this is the program's output: a number.
+        
         return s
