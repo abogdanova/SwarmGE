f43e70edf620867e7865fb4dbb7688aa8e2dcf78
mikefenton <michaelfenton1@gmail.com>
2017-02-16 13:31:16 +0000

    Fixed typo in Individual.py

--- src/representation/individual.py
+++ src/representation/individual.py
@@ -96,7 +96,7 @@ class Individual(object):
         # Create a copy of self by initialising a new individual.
         new_ind = Individual(list(self.genome), new_tree, map_ind=False)
 
-        # Set new individaul parameters (no need to map genome to new
+        # Set new individual parameters (no need to map genome to new
         # individual).
         new_ind.phenotype, new_ind.invalid = self.phenotype, self.invalid
         new_ind.depth, new_ind.nodes = self.depth, self.nodes
