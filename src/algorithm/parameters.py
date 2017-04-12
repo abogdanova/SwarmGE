d2cff640b576804a42137c36b256a6dbf46a0bd0
dvpfagan <fagan.david@gmail.com>
2017-04-12 14:27:29 +0100

    Set hard limit for max tree depth due to python eval() having a stack limit of 92-99 in size

--- src/algorithm/parameters.py
+++ src/algorithm/parameters.py
@@ -58,7 +58,7 @@ params = {
         'TARGET': "ponyge_rocks",
 
         # Set max sizes of individuals
-        'MAX_TREE_DEPTH': None,
+        'MAX_TREE_DEPTH': 90,  #SET TO 90 DUE TO PYTHON EVAL() STACK LIMIT - GO HIGHER AT YOUR OWN RISK
         'MAX_TREE_NODES': None,
         'CODON_SIZE': 100000,
         'MAX_GENOME_LENGTH': None,
