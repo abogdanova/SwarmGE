51bc05fd89ae5b9857d3f4c0fed3859e9997c93b
Brendan (dhcp-892b6a81) <codykenny@gmail.com>
2017-06-13 16:41:00 +0100

    Changed match object to use pythons re MATCH object

--- src/fitness/regextesting/RegexTest.py
+++ src/fitness/regextesting/RegexTest.py
@@ -1,3 +1,4 @@
+import re
 
 
 class RegexTest:
@@ -5,19 +6,21 @@ class RegexTest:
     Class which contains a test string and matches
     (We should probably just use the types which re library uses)
     """
-        
+
     def __init__(self, search_string):
-        
+
         # print("Added regex search string: "+search_string)
         self.search_string = search_string
         self.matches = []
 
-    def add_match(self, start, end):
-        
-        self.matches.append({'start': start,
-                             'end': end,
-                             'matched_string': self.search_string[start:end]})
-        
+    def add_re_match(self, re_match):
+        self.matches.append(re_match)
+
+#    def add_match(self, start, end):
+#        self.matches.append({'start': start,
+#                             'end': end,
+#                             'matched_string': self.search_string[start:end]})
+
     def calc_match_errors(self, match_candidates):
 
         undesired_range = missing_range = 0
@@ -36,41 +39,42 @@ class RegexTest:
         return match_error
 
     def get_search_string(self):
-        
         return self.search_string
 
     def find_missing_range(self, a_known_match, match_ranges):
-        start = a_known_match.get("start")
-        end = a_known_match.get("end")
+        # a_match.start(), a_match.end()
+        start = a_known_match.start()  # get("start")
+        end = a_known_match.end()  # get("end")
         missing = end - start
-        
+
         for i in range(start, end):
             found = False
-            
+
             for m_range in match_ranges:
-                
+
                 if m_range.start() <= i < m_range.end():
                     found = True
-            
+
             if found:
                 missing -= 1
-        
+
         return missing
 
     def find_undesired_range(self, match_candidate, known_matches):
+        # a_match.start(), a_match.end()
         undesired_matched = 0
-        
+
         for i in range(match_candidate.start(), match_candidate.end()):
             in_range = False
-        
+
             for a_known_match in known_matches:
-                start = a_known_match.get("start")
-                end = a_known_match.get("end")
-        
+                start = a_known_match.start()  # get("start")
+                end = a_known_match.end()  # get("end")
+
                 if start <= i <= end:
                     in_range = True
-            
+
             if not in_range:
                 undesired_matched += 1
-        
+
         return undesired_matched
--- src/fitness/regextesting/RegexTestGenerator.py
+++ src/fitness/regextesting/RegexTestGenerator.py
@@ -9,7 +9,7 @@ Relies on regex evaluator.
 (Which relies on this class. TODO remove circular dependency by calling test generation during PonyGE2 initialisation instead of during RegexEval class instantiation.)
 
 """
-  
+
 
 def generate_equivalence_test_suite_replacement(a_match, compiled_regex):
     """
@@ -20,7 +20,7 @@ def generate_equivalence_test_suite_replacement(a_match, compiled_regex):
     define (or outline) the functionality boundaries of the regex. When we
     go to evolve new regexs, we can use the test suite to measure
     functionality equivalence with the original test regex.
-        
+
     :param a_match:
     :param compiled_regex:
     :return:
@@ -34,20 +34,20 @@ def generate_equivalence_test_suite_replacement(a_match, compiled_regex):
             for char in [a for a in range(ord('0'), ord('9'))] + \
                     [ord('a'), ord('Z')]:
                 new_search_string = a_match.search_string[:i] + \
-                                    chr(char) + \
-                                    a_match.search_string[i+1:]
+                    chr(char) + \
+                    a_match.search_string[i + 1:]
                 a_test_case_string = RegexTest(new_search_string)
                 vals = time_regex_test_case(compiled_regex,
-                                                 a_test_case_string, 1)
+                                            a_test_case_string, 1)
                 if len(list(vals[1])) == 0:
                     test_cases.append(a_test_case_string)
     return test_cases
 
 
-def generate_equivalence_test_suite_length( a_match, compiled_regex):
+def generate_equivalence_test_suite_length(a_match, compiled_regex):
     """
     Generate shorter or longer test cases, add those which do not match
-    
+
     :param a_match:
     :param compiled_regex:
     :return:
@@ -60,18 +60,20 @@ def generate_equivalence_test_suite_length( a_match, compiled_regex):
 
         # check string with one character added at the front
         new_search_string = 'a' + a_match.search_string
-        
+
         add_test_case_if_fails(new_search_string, compiled_regex, test_cases)
         # check string with one character added at the end
-        new_search_string = a_match.search_string+'a'
+        new_search_string = a_match.search_string + 'a'
         add_test_case_if_fails(new_search_string, compiled_regex, test_cases)
-        for i in range(len(a_match.search_string)-1):
-            new_search_string = a_match.search_string[i:]  # TODO: refactor this
+        for i in range(len(a_match.search_string) - 1):
+            # TODO: refactor this
+            new_search_string = a_match.search_string[i:]
             add_test_case_if_fails(new_search_string, compiled_regex,
                                    test_cases)
-        
-        for i in range(len(a_match.search_string)-1):
-            new_search_string = a_match.search_string[:i]  # TODO: refactor this
+
+        for i in range(len(a_match.search_string) - 1):
+            # TODO: refactor this
+            new_search_string = a_match.search_string[:i]
             add_test_case_if_fails(new_search_string, compiled_regex,
                                    test_cases)
     return test_cases
@@ -80,7 +82,7 @@ def generate_equivalence_test_suite_length( a_match, compiled_regex):
 def add_test_case_if_fails(new_search_string, compiled_regex, test_cases):
     """
     run a test case, if it fails, add it to the suite of tests
-    
+
     :param new_search_string:
     :param compiled_regex:
     :return:
@@ -91,109 +93,109 @@ def add_test_case_if_fails(new_search_string, compiled_regex, test_cases):
         test_cases.append(a_test_case_string)
 
 
-def add_test(regex_string, match, test_cases):
-    """
-    Add a test safely
-    
-    :param regex_string:
-    :param match:
-    :return:
-    """
-    a_test_string = RegexTest(regex_string)
-    if match and len(match) == 2:  # should be e.g. [0,27]
-        a_test_string.add_match(match[0], match[1])
-    test_cases.append(a_test_string)
-    return a_test_string
-        
+# def add_test(regex_string, match, test_cases):
+#    """
+#    Add a test safely
+#
+#    :param regex_string:
+#    :param match:
+#    :return:
+#    """
+#    a_test_string = RegexTest(regex_string)
+#    if match and len(match) == 2:  # should be e.g. [0,27]
+#        a_test_string.add_re_match(match)
+#    test_cases.append(a_test_string)
+#    return a_test_string
+
 
 def generate_test_suite(regex_string, session):
     # do some test generation
     # find a string which the regex is able to match against
     # find the minimal variant of this string which does not match
     # test strategies - length, values
-    
+
     # collect strings which identify the different regex
     # cache and reuse these (read/write to a file before/after GP)
-    known_test_strings=["5C0A5B634A82",
-                        "Jan 12 06:26:20: ACCEPT service dns from 140.105.48.16 to firewall(pub-nic-dns), prefix: \"none\" (in: eth0 140.105.48.16(00:21:dd:bc:95:44):4263 -> 140.105.63.158(00:14:31:83:c6:8d):53 UDP len:76",
-                        "Jan 12 06:27:09: DROP service 68->67(udp) from 216.34.211.83 to 216.34.253.94, prefix: \"spoof iana-0/8\" (in: eth0 213.92.153.78(00:1f:d6:19:0a:80):68 -> 69.43.177.110(00:30:fe:fd:d6:51):67 UDP le"
-                        "Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: ",
-                        "Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: ",
-                        "26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: ",
-                        " -> 140.105.63.164(50:j6:04:92:53:44):80 TCP flags: ****S* len:60 ttl:32)sdkfjhaklsjdhfglksjhdfgk",
-                        " -> 140.105.63.16(50:06:04:9r:53:44):80 TCP flags: ****S* len:60 ttl:32)ssjdhfglksjhdfgk",
-                        "Jan 12 06:26:20: ACCEPT service dns from 140.105.48.16 to firewall(pub-nic-dns), prefix: ",
-                        "Jan 12 06:27:09: DROP service 68->67(udp) from 216.34.211.83 to 216.34.253.94, prefix: ",
-                        "105.63.1650:06:04:92:53:44:80",
-                        " -> 140.105.63.164(50:06:g4:92:53:44):80 TCP flags: ****S* len:60 ttl:32)",
-                        " -> 140.105.63.164(50:06:54:92:r3:44):80 TCP flags: ****S* len:60 ttl:32)",
-                        "1,2,3,4,5,6,7,8,9,10,11,12,13777,5P,5,5,6,5P",
-                        "1,2,3,4,5,6,7,8,9,10,11,12,13777,24,5P",
-                        "1,2,3,4,5,6,7,8,9,10,11,12,13777,243,3P",
-                        "1,2,3,4,5,6,7,8,9,10,11,12,13777,P",
-                        "1,2,3,4,5,6,7,8,9,10,11,12,P",
-                        "1,2,3,4,5,6,7,8,9,10,11,P",
-                        "1,2,3,4,5,6,7,8,9,10,11,3P",
-                        "1,2,3,4,5,6,7,8,9,10,P",
-                        "codykenny@gmailcom",
-                        "2016-12-09T08:21:15.9+00:00",
-                        "2016-12-09T08:21:15.9+00:0",
-                        "2016-22-09T08:21:15.9+00:00000000000",
-                        "2016-22-09T08:21:15.9+00:00",
-                        "1911-02-19T22:35:42.3+08:43",
-                        "2016-09-05T15:22:26.286Z",
-                        "230.234E-10",
-                        "971.829E+26",
-                        "3566",
-                        "4",
-                        "-7",
-                        "+94",
-                        "            36",
-                        "78      ",
-                        "87465.345345",
-                        "2346.533",
-                        "0.045e-10",
-                        "3566.",
-                        ".3456",
-                        "<string> ::= <letter>|<letter><string>",
-                        "hryxioXcXXdornct",
-                        "bbbbXcyXXaaa",
-                        "230.234E-10",
-                        "971.829E+26",
-                        "3566",
-                        "4",
-                        "-7",
-                        "+94",
-                        "            36",
-                        "78      ",
-                        "87465.345345",
-                        "2346.533",
-                        "  3566.   ",
-                        " .3456  ",
-                        "a46b  ",
-                        "0.045e-10",
-                        "aXXXXas",
-                        "<s_char>        ::= !|\"#\"|$|%|&|\(|\)|*|+|,|-|.|\/|:|;|\"<\"|=|\">\"|?|@|\[|\\|\]|^|_|\"`\"|{|}|~|\"|\"|'\"'|\"'\"|\" \""
-                        "!|\"#\"|$|%|&|\(|\)|*|+|,|-|.|\/|:|;|\"<\"|=|\">\"|?|@|\[|\\|\]|^|_|\"`\"|{|}|~|\"|\"|'\"'|\"'\"|\" \"",
-                        "<A_Z>           ::= A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z",
-                        "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z",
-    ]
-    
+    known_test_strings = ["5C0A5B634A82",
+                          "Jan 12 06:26:20: ACCEPT service dns from 140.105.48.16 to firewall(pub-nic-dns), prefix: \"none\" (in: eth0 140.105.48.16(00:21:dd:bc:95:44):4263 -> 140.105.63.158(00:14:31:83:c6:8d):53 UDP len:76",
+                          "Jan 12 06:27:09: DROP service 68->67(udp) from 216.34.211.83 to 216.34.253.94, prefix: \"spoof iana-0/8\" (in: eth0 213.92.153.78(00:1f:d6:19:0a:80):68 -> 69.43.177.110(00:30:fe:fd:d6:51):67 UDP le"
+                          "Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: ",
+                          "Jan 12 06:26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: ",
+                          "26:19: ACCEPT service http from 119.63.193.196 to firewall(pub-nic), prefix: ",
+                          " -> 140.105.63.164(50:j6:04:92:53:44):80 TCP flags: ****S* len:60 ttl:32)sdkfjhaklsjdhfglksjhdfgk",
+                          " -> 140.105.63.16(50:06:04:9r:53:44):80 TCP flags: ****S* len:60 ttl:32)ssjdhfglksjhdfgk",
+                          "Jan 12 06:26:20: ACCEPT service dns from 140.105.48.16 to firewall(pub-nic-dns), prefix: ",
+                          "Jan 12 06:27:09: DROP service 68->67(udp) from 216.34.211.83 to 216.34.253.94, prefix: ",
+                          "105.63.1650:06:04:92:53:44:80",
+                          " -> 140.105.63.164(50:06:g4:92:53:44):80 TCP flags: ****S* len:60 ttl:32)",
+                          " -> 140.105.63.164(50:06:54:92:r3:44):80 TCP flags: ****S* len:60 ttl:32)",
+                          "1,2,3,4,5,6,7,8,9,10,11,12,13777,5P,5,5,6,5P",
+                          "1,2,3,4,5,6,7,8,9,10,11,12,13777,24,5P",
+                          "1,2,3,4,5,6,7,8,9,10,11,12,13777,243,3P",
+                          "1,2,3,4,5,6,7,8,9,10,11,12,13777,P",
+                          "1,2,3,4,5,6,7,8,9,10,11,12,P",
+                          "1,2,3,4,5,6,7,8,9,10,11,P",
+                          "1,2,3,4,5,6,7,8,9,10,11,3P",
+                          "1,2,3,4,5,6,7,8,9,10,P",
+                          "codykenny@gmailcom",
+                          "2016-12-09T08:21:15.9+00:00",
+                          "2016-12-09T08:21:15.9+00:0",
+                          "2016-22-09T08:21:15.9+00:00000000000",
+                          "2016-22-09T08:21:15.9+00:00",
+                          "1911-02-19T22:35:42.3+08:43",
+                          "2016-09-05T15:22:26.286Z",
+                          "230.234E-10",
+                          "971.829E+26",
+                          "3566",
+                          "4",
+                          "-7",
+                          "+94",
+                          "            36",
+                          "78      ",
+                          "87465.345345",
+                          "2346.533",
+                          "0.045e-10",
+                          "3566.",
+                          ".3456",
+                          "<string> ::= <letter>|<letter><string>",
+                          "hryxioXcXXdornct",
+                          "bbbbXcyXXaaa",
+                          "230.234E-10",
+                          "971.829E+26",
+                          "3566",
+                          "4",
+                          "-7",
+                          "+94",
+                          "            36",
+                          "78      ",
+                          "87465.345345",
+                          "2346.533",
+                          "  3566.   ",
+                          " .3456  ",
+                          "a46b  ",
+                          "0.045e-10",
+                          "aXXXXas",
+                          "<s_char>        ::= !|\"#\"|$|%|&|\(|\)|*|+|,|-|.|\/|:|;|\"<\"|=|\">\"|?|@|\[|\\|\]|^|_|\"`\"|{|}|~|\"|\"|'\"'|\"'\"|\" \""
+                          "!|\"#\"|$|%|&|\(|\)|*|+|,|-|.|\/|:|;|\"<\"|=|\">\"|?|@|\[|\\|\]|^|_|\"`\"|{|}|~|\"|\"|'\"'|\"'\"|\" \"",
+                          "<A_Z>           ::= A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z",
+                          "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z",
+                          ]
+
     compiled_regex = re.compile(regex_string)
     test_cases = []
     for test_string in known_test_strings:
         test_cases += generate_tests_if_string_match(compiled_regex,
                                                      test_string)
-    
+
     # if we don't have any known test strings, see if the regex matches it.
     test_cases += generate_tests_if_string_match(compiled_regex, regex_string)
-    
+
     print("Number of test cases in suite:", len(test_cases))
-         
+
     if len(test_cases) == 0:
         raise ValueError("PonyGE2 couldn't find any passing test cases "
                          "for this regex!")
-    
+
     # else:
     #     for i, case in enumerate(test_cases):
     #         if not case.matches:
@@ -211,18 +213,19 @@ def generate_test_suite(regex_string, session):
     #                 session.add(new_test_case)
 
     return test_cases
-            
+
+
 def add_re_match_to_test(vals, passing_test_string):
     """
     take matching values as found by the regex library, and add them to our
     RegexTest object
-    
+
     :param vals:
     :param passing_test_string:
     :return:
     """
     for a_match in vals[1]:  # this vals[1] business is not good
-        passing_test_string.add_match(a_match.start(), a_match.end())
+        passing_test_string.add_re_match(a_match)
     return passing_test_string
 
 
@@ -230,14 +233,14 @@ def generate_tests_if_string_match(compiled_regex, test_string):
     test_cases = []
     a_test_candidate = RegexTest(test_string)
     vals = time_regex_test_case(compiled_regex, a_test_candidate, 1)
-    
+
     if len(list(vals[1])) > 0:  # the regex found a match, add it
         a_positive_test = add_re_match_to_test(vals, a_test_candidate)
         test_cases.append(a_positive_test)
-        
+
         # now find regex which negate
         test_cases += generate_equivalence_test_suite_replacement(a_positive_test,
-                                                            compiled_regex)
+                                                                  compiled_regex)
         test_cases += generate_equivalence_test_suite_length(a_positive_test,
-                                                            compiled_regex)
+                                                             compiled_regex)
     return test_cases
