ca317c9ccf77304478fc4c60fba2b953a153413c
mikefenton <michaelfenton1@gmail.com>
2017-06-27 15:30:17 +0100

    Added a simple grammar analyser script that will print out the number of unique permutations and combinations possible for a specified grammar at a ramping range of depths.

---
+++ src/scripts/grammar_analyser.py
@@ -0,0 +1,50 @@
+from utilities.algorithm.general import check_python_version
+
+check_python_version()
+
+from algorithm.parameters import params
+import utilities.algorithm.command_line_parser as parser
+from representation.grammar import Grammar
+
+import sys
+import os
+
+
+def main(command_line_args):
+    """
+    Given a specified grammar file, parse the grammar using the Grammar class
+    and print out the number of unique permutations and combinations of
+    distinct phenotypes that this grammar is capable of generating at a
+    number of given depths.
+    
+    :return: The grammar.permutations dictionary.
+    """
+
+    # Parse command line args (we only want the grammar file)
+    cmd_args, unknown = parser.parse_cmd_args(command_line_args)
+
+    # Join original params dictionary with command line specified arguments.
+    # NOTE that command line arguments overwrite all previously set parameters.
+    params.update(cmd_args)
+
+    # Parse grammar file and set grammar class.
+    grammar = Grammar(os.path.join("..", "..", "grammars",
+                                   params['GRAMMAR_FILE']))
+    
+    print("\nUnique permutations and combinations of possible solutions"
+          " for a range of depths for specified grammar:",
+          params['GRAMMAR_FILE'], "\n")
+    
+    for depth in grammar.permutations:
+        
+        print(" Depth: %d \t Permutations: %d" %
+              (depth, grammar.permutations[depth]))
+        
+
+if __name__ == "__main__":
+    
+    # Do not write or save any files.
+    params['DEBUG'] = True
+    
+    # Run main program.
+    main(sys.argv[1:])  # exclude the ponyge.py arg itself
